457f20d25252a636467f3d713ff82633c47495cd
harleylizard <64570529+harleylizard@users.noreply.github.com>
2024-04-20 02:03:15 +1000

    better dithering

--- src/main/resources/shaders/model_fragment.glsl
+++ src/main/resources/shaders/model_fragment.glsl
@@ -1,13 +1,14 @@
 #version 460 core
 
-const float constant = 1.0F;
-const float linear = 0.09F;
-const float quadratic = 0.032F;
+const float CONSTANT = 1.0F;
+const float LINEAR = 0.09F;
+const float QUADRATOC = 0.032F;
 
-const mat3 bayerMatrix = mat3(
-    vec3(0.0F, 32.0F, 8.0F),
-    vec3(48.0F, 16.0F, 40.0F),
-    vec3(12.0F, 44.0F, 4.0F)
+const mat4 BAYER_MATRIX = mat4(
+0.0F,  8.0F,  2.0F, 10.F,
+12.0F,  4.0F, 14.0F,  6.0F,
+3.0F, 11.0F,  1.0F,  9.0F,
+15.0F,  7.0F, 13.0F,  5.0F
 );
 
 out vec4 color;
@@ -19,8 +20,8 @@ in vec4 m_lightMap;
 
 uniform sampler2DArray tSampler;
 
-vec3 normalMap = texture(tSampler, vec3(m_uv.x, m_uv.y, m_uv.z + 1)).xyz;
-vec3 emissiveMap = texture(tSampler, vec3(m_uv.x, m_uv.y, m_uv.z + 2)).xyz;
+vec3 normalMap = texture(tSampler, vec3(m_uv.xy, m_uv.z + 1)).xyz;
+vec3 emissiveMap = texture(tSampler, vec3(m_uv.xy, m_uv.z + 2)).xyz;
 
 uniform int size;
 
@@ -33,18 +34,19 @@ layout (binding = 0) buffer lightsBuffer {
     Light lights[];
 };
 
-vec4 spherical(Light light) {
+vec4 createPointLight(Light light, mat3 tbn) {
     vec3 magnitude = light.position.xyz - m_position.xyz;
 
     float distance = length(magnitude);
-    float attenuation = 1.0F / ((constant + linear) * distance + quadratic * distance * distance);
+    float attenuation = 1.0F / ((CONSTANT + LINEAR) * distance + QUADRATOC * distance * distance);
 
-    vec3 transformed = normalize(m_normal);
+    // Normalise between -1 and 1.
+    vec3 transformed = tbn * (normalMap * 2.0F - 1.0F);
 
     float direction = dot(transformed, normalize(magnitude));
 
     float radius = 16.0F;
-    float intensity = max(direction, 0.0F) * (radius * light.color.w);
+    float intensity = direction * (radius * light.color.w);
 
     return vec4((light.color.rgb * intensity) * attenuation, 1.0F);
 }
@@ -58,38 +60,53 @@ vec4 minVec4(vec4 l, vec4 r) {
 }
 
 vec4 ditherColor(vec4 color) {
-    float grayscale = dot(color.rgb, vec3(0.299F, 0.587F, 0.114F));
+    float grayscale = dot(color.rgb, vec3(0.2126F, 0.7152F, 0.0722F));
 
-    vec2 size = vec2(gl_FragCoord.x, gl_FragCoord.y) * 1.0F;
-    vec3 offset = vec3(
-        bayerMatrix[int(mod(size.y, 3.0F))][int(mod(size.x, 3.0F))],
-        bayerMatrix[int(mod(size.y + 1.0F, 3.0F))][int(mod(size.x + 1.0F, 3.0F))],
-        bayerMatrix[int(mod(size.y + 2.0F, 3.0F))][int(mod(size.x + 2.0F, 3.0F))]
-    )* (1.0F / 64.0F);
+    vec2 ditherPos = fract(gl_FragCoord.xy / 4.0F);
+    grayscale += (BAYER_MATRIX[int(ditherPos.x)][int(ditherPos.y)] - 7.5F) / 16.0F;
+
+    vec2 fragCoords = mod(gl_FragCoord.xy, 4.0F);
+    vec3 offset = vec3(BAYER_MATRIX[int(fragCoords.x)][int(fragCoords.y)] / 16.0F);
 
     vec3 ditheredColor = color.rgb + (offset * (grayscale - 0.5F));
-    return vec4(ditheredColor, 1.0F);
+    return vec4(ditheredColor, 1.0);
+}
+
+
+// Tangent, Bitangent, Normal matrix.
+mat3 getTBN() {
+    vec3 edge0 = dFdx(m_position.xyz);
+    vec3 edge1 = dFdy(m_position.xyz);
+    vec2 delta0 = dFdx(m_uv.xy);
+    vec2 delta1 = dFdy(m_uv.xy);
+
+    float det = 1.0F / (delta0.x * delta1.y - delta1.x * delta0.y);
+    vec3 tangent = vec3(det * (delta1.y * edge0 - delta0.y * edge1));
+    vec3 bitangent = vec3(det * (-delta1.x * edge0 + delta0.x * edge1));
+    return mat3(tangent, bitangent, m_normal);
 }
 
 void main() {
-    vec4 pixel = texture(tSampler, vec3(m_uv)) * vec4(1.0F, 1.0F, 1.0F, 1.0F);
-    if (pixel.a == 0) {
+    vec4 pixel = texture(tSampler, vec3(m_uv));
+    if (pixel.a == 0) { // Transparent textures.
         discard;
     }
     vec4 result = vec4(0.0F);
 
+    mat3 tbn = getTBN();
     for(int i = 0; i < size; ++i) {
-        result += spherical(lights[i]);
+        result += createPointLight(lights[i], tbn);
     }
+    // Making sure the light doesn't become completely white when adding low light levels.
     result = minVec4(result, vec4(m_lightMap.rgb * 1.25F, 1.0F));
 
-    float brightness = 0.75F;
+    float brightness = 1.25F;
     color = ditherColor(pixel * vec4(result.rgb * brightness, 1.0F));
 
-    if (m_normal.xyz == 0) {
-        color = pixel * m_lightMap;
+    if (m_normal.xyz == 0) { // Ambient models
+        color = pixel * vec4(m_lightMap.rgb, 1.0F);
     }
-    if (emissiveMap.r > 0) {
-        color = pixel;
+    if (emissiveMap.r > 0) { // Emissive textures.
+        color = ditherColor(pixel);
     }
 }
